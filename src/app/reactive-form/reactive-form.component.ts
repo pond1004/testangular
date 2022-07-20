33765f4a0489b455b644a312ac8f9b6f142210f2
PirapatR <108516431+Pirapatr1999@users.noreply.github.com>
2022-07-20 16:44:39 +0700

    [PirapatR] test reactive-form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder,FormArray,FormControl } from '@angular/forms';
+import {count} from "rxjs/operators";
+// import Form ต่างๆมาใช่งาน จาก '@angular/forms'
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
-
-  constructor() { }
+  // กำหนดตัวแปล FormGroup:A
+  A: FormGroup;
+  constructor(
+    private FB : FormBuilder
+  ) {
+  //  กำหนด FormGroup:A = FormBuilder:FB
+    this.A = this.FB.group({
+    // กำหนดตัวแปล FormControlName ที่ี FormGroup รับมา
+      fistname:'',
+      lastname:'',
+    });
+  //  FormGroup:A .valueChanges.Subscribe ส่งค่าผ่าน console.log
+    this.A.valueChanges.subscribe(console.log);
+  }
 
   ngOnInit(): void {
   }
-
-}
+//  method B รับการกระทำจาก ปุ่ม Send Form-A
+  B():void{
+    console.log(this.A.value);
+    // หลังจากกด method B จะทำการ console.log ค่า FormGroup.A
+    }
+  }
