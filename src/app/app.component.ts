c51f4c27775e1577794864efe20e38940983fd12
pond1004 <s6104022610032@email.kmutnb.ac.th>
2022-07-20 14:10:57 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// เรียกใช้งาน FormBuilder และ FormGroup
+import { FormGroup, FormBuilder } from '@angular/forms';
+/*
+FormGroup import มาเพื่อเก็บข้อมูลของฟอร์มในรูปแบบกลุ่ม
+FormBuilder import มาเพื่อที่จะใช้งาน Group เพื่อจะgroupข้อมูลภายในฟอร์ม
+*/
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent implements OnInit {
+  // สร้างตัวแปรมาชื่อว่า myForm โดยเก็บเป็นรูปแบบ FormGroup
+
+   title = 'test';
+  myForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.myForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: ''
+    });
+    this.myForm.valueChanges.subscribe(console.log);
+  }
+
+  ngOnInit() {}
 }
