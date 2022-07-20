0b1ad7b0e6f3531104537d0e6d8c179c00a9fb57
PirapatR <108516431+Pirapatr1999@users.noreply.github.com>
2022-07-20 14:51:04 +0700

    [PirapatR] add new component reactive-form.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
